3a0a24b64df7f59516fb8dfd192253051b7f1745
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2026-05-12 10:45:09 +0200

    add game ui

---
+++ web-app/app/game/game.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import Board from "@/app/game/board";
+
+export default function Game() {
+
+    const startListenOnWebsocket = () => {
+        const socket = new WebSocket('ws://localhost:8080/ws');
+        socket.addEventListener('open', function (event) {
+            console.log('WebSocket connection opened');
+        });
+        socket.addEventListener('message', function (event) {
+            console.log('Message from server ', event.data);
+            log.push((new Date()).toString() + "> " + event.data)
+        });
+    }
+
+    const log: Array<string> = [];
+
+    const logEntries = log.map((log) => {
+        return <span className="log-entry">{log}</span>
+    })
+
+    return <div className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between">
+
+        <button onClick={startListenOnWebsocket}>Start listening on WebSocket</button>
+
+        <Board/>
+
+        {logEntries}
+    </div>
+}
